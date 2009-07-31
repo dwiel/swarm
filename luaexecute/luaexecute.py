e728a9fdb41d7e2cf2808884ca68234acbe33cb9
dwiel <zdwiel@gmail.com>
2009-07-30 22:10:46 -0400

    added luaexecute.py for a real time command line like interface

---
+++ luaexecute/luaexecute.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import osc
+import sys
+
+osc.init()
+
+#for line in sys.stdin :
+lua = ""
+print "enter command:"
+while lua != "quit\n" and lua != "exit\n":
+	line = sys.stdin.readline()
+	lua += line
+	if line == "" or line == "\n" :
+		bundle = osc.createBundle()
+		# osc.appendToBundle(bundle, "/lua/execute", 'groups["swarm1"].pos.z = -30')
+		osc.appendToBundle(bundle, "/lua/execute", lua)
+		osc.sendBundle(bundle, "localhost", 9001)
+		print "sent:\n  >" + lua.replace('\n', '\n  >')
+		print "------------"
+		print "enter command:"
+		lua = ""
+
+
+
